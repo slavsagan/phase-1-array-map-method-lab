d065880c19c15819a1d6f4d00101bc4c68f31798
Slav <itiksolution@gmail.com>
2022-10-10 15:29:03 +1100

    assignment complete

--- index.js
+++ index.js
@@ -8,9 +8,21 @@ const tutorials = [
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+  'what is JSONP?',
+]
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  const newArr = [...tutorials]
+
+  for (let i = 0; i < newArr.length; i++) {
+    let words = newArr[i].split(' ')
+
+    for (let index = 0; index < words.length; index++) {
+      words[index] = words[index][0].toUpperCase() + words[index].slice(1)
+    }
+    newArr[i] = words.join(' ')
+  }
+  return newArr
 }
+console.log(titleCased())
